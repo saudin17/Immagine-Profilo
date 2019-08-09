3150a8d9b0f36345cebf535a609abf7e18326029
saudin17 <53856989+saudin17@users.noreply.github.com>
2019-08-09 09:47:29 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import sys, os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+sys.path.append(os.path.abspath(os.pardir))
+
+__version__ = '1.0'
+
+# -- General configuration -----------------------------------------------------
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'immagine profilo'
+copyright = '2017, WOW WOW'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+extlinks = {}
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = 'default'
+
+html_static_path = ['static']
+
+def setup(app):
+    # overrides for wide tables in RTD theme
+    app.add_stylesheet('theme_overrides.css') # path relative to static
+
+"""
+  You might want to uncomment the “latex_documents = []” if you use CKJ characters in your document.
+  Because the pdflatex raises exception when generate Latex documents with CKJ characters.
+"""
+#latex_documents = []
+
+# inserire un logo in alto a sinistra (mettendo l’immagine nella cartella “static”)
+latex_logo = "static/immagine_1.png"
+html_logo = "static/immagine_1.png"
